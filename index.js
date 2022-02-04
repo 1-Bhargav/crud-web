69a9c7b2e9a280f1e52653d35b6f25fdeff23e9d
Bhargav Chauhan <86279739+1-Bhargav@users.noreply.github.com>
2022-02-04 08:44:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose')
+var express = require('express')
+var route = require('./routes')
+var bodyParser =require('body-parser')
+
+const port = process.env.PORT || 3000
+
+const db = 'mongodb+srv://Bhargav:<password>@cluster0.zlqm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+
+mongoose.connect(db).then(()=>{
+    console.log('DB Connected')
+
+    app = express();
+    app.use(bodyParser.urlencoded({extended:false}))
+    app.use('/api',route)
+    
+    app.listen(port,()=>{
+        console.log('Server Started At Port 3000')
+    })
+}).catch((e)=>{
+    console.log(e.toString())
+})
---
+++ routes.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+var book = require('./Models/books')
+
+router.get('/books',async(req,res)=>{
+   
+    const ibook = await book.find()
+    res.send(ibook)
+
+})
+
+router.post('/books', async(req,res) => {
+    
+    const ibook = new book({
+        name: req.body.name,
+        author: req.body.author
+    })
+
+    await ibook.save((err,msg)=>{
+
+        if(err){
+            res.status(500).json({
+                "error":err
+            })
+        }
+        else{
+            res.status(200).json({
+                "message":msg
+            })
+        }
+    })
+})
+
+router.patch('/books/:id', async(req,res) => {
+    console.log("dsdfs")
+    
+    const ibook = await book.findOne({_id:req.params.id})
+    ibook.name = req.body.name
+    ibook.author = req.body.author
+
+    await ibook.save((err,msg) => {
+        
+        if(err){
+            res.status(500).json({
+                "error":err
+            })
+        }
+
+        else{
+            res.status(200).json({
+                "message":msg
+            })
+        }
+    })
+})
+
+router.delete("/books/:id",async(req,res)=>{
+    
+    const _id = req.params.id;
+    
+    const book = await Song.findByIdAndDelete(_id);
+    
+    res.send(ibook);
+
+  });
+
+
+
+
+module.exports = router
